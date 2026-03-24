a7385c4b93b765c2cde36dc2c33092cf02ce8ffe
vedsu <90701239+vedsu@users.noreply.github.com>
2026-03-24 11:25:54 +0530

    Refactor app initialization and change port to 5000

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-# root/app.py
-from app import app
+from app import create_app
+
+app = create_app()
+
 
 if __name__ == "__main__":
-    # This part only runs when you run "python app.py" locally
-    # App Runner will use Gunicorn instead
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host="0.0.0.0", port=5000)
