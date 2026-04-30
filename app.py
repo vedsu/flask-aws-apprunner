841c6432bcdcdbb45e6d0f782ff7986b0f48a87e
vedsu <90701239+vedsu@users.noreply.github.com>
2026-04-30 15:37:32 +0530

    Enable debug mode for application run
    Updated the application run configuration to enable debug mode.

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from app import create_app
+# run.py
+from app import app
 
-app = create_app()
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+# Run the application
+if __name__=="__main__":
+    app.run(debug=True, host="0.0.0.0")
